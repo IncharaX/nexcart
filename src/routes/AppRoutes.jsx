5e3e3e47a5457fd85b5911dd197c4e6d6581ade6
Inchara nk <incharank1@gmail.com>
2026-06-07 22:07:46 +0530

    first commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
